be91464e9f5a0c7a2c840a5dfb360936251ba389
muhamad_emir <muhamademiralfani@gmail.com>
2023-11-25 06:54:27 +0700

    perubahan ke limat

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
-import Item from "./TodoItem";
+import TodoItem from "./TodoItem";
+
 
 export default class TodoList extends Component {
   render() {
+    const { item, clearList, handleDelete, handleEdit } = this.props;
     return (
-      <section>
-        <h2>todo llist</h2>
-        <Item />
-      </section>
+      <ul className="list-group my-5">
+        <h3 className="text-capitalize text-center">todo list</h3>
+        {item.map(item => {
+          return (
+            <TodoItem
+              key={item.id}
+              title={item.title}
+              handleDelete={() => handleDelete(item.id)}
+              handleEdit={() => handleEdit(item.id)}
+            />
+          );
+        })};
+        <button className="btn btn-danger btn-block text-uppercase mt-5"
+          onClick={clearList}
+        >
+          Clear List
+        </button>
+      </ul>
     );
   }
 }
