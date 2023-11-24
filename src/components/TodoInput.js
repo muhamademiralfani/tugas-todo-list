ab26ef173e413a9948bc8abd423b9b93fa1aa81c
muhamad_emir <muhamademiralfani@gmail.com>
2023-11-24 20:10:24 +0700

    perubahan ketiga

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,7 +1,29 @@
 import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
 
 export default class TodoInput extends Component {
   render() {
-    return <div>TodoInput</div>;
+    const { item, handleChange, handleSubmit, editItem } = this.props;
+    return (
+      <div className="card card-body my-3">
+        <Form action="" onSubmit={handleSubmit}>
+          <div className="input-group">
+            <InputGroup className="input-group-prepend">
+              <input
+                type="text"
+                className="form-control text-capitalize shadow-sm"
+                placeholder="add todo item"
+                value={item}
+                onChange={handleChange}
+              />
+              <button type="submit" className="btn btn-primary btn-lg">
+                add item
+              </button>
+            </InputGroup>
+          </div>
+        </Form>
+      </div>
+    );
   }
 }
